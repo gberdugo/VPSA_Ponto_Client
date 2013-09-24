804fca882ae7f5a78442d6b2bc4588faa39c780f
Gustavo Berdugo <gberdugo@gmail.com>
2013-09-23 22:24:42 -0300

    Adição da biblioteca de seriallcd

--- VPSA_Ponto_Client.ino
+++ VPSA_Ponto_Client.ino
@@ -4,37 +4,21 @@
 #include <DS1307.h>
 #include <SoftwareSerial.h>
 #include <SeeedRFIDLib.h>
-#include <LiquidCrystal.h>
+#include <SerialLCD.h>
 
 /* LCD Configuration:
- * LCD RS pin to digital pin 7
- * LCD Enable pin to digital pin 6
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 1
- * LCD D7 pin to digital pin 0
- * LCD R/W pin to ground
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
+ * Pin 2 and 3
 */
-#define LCD_RS	  7
-#define LCD_E	  6
-#define LCD_D4	  5
-#define LCD_D5	  4
-#define LCD_D6	  1
-#define LCD_D7	  0
-#define LCD_COLS 16
-#define LCD_ROWS  2
-
-LiquidCrystal lcd(LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
+#define LCD_PIN1  2
+#define LCD_PIN2  3
+SerialLCD lcd(LCD_PIN1, LCD_PIN2);
 
 /* RFID Reader Configuration:
- * Reader RX pin to pin 2
- * Reader TX pin to pin 3
+ * Reader RX pin to pin 1
+ * Reader TX pin to pin 0
 */
-#define RFID_RX_PIN 2
-#define RFID_TX_PIN 3
+#define RFID_RX_PIN 1
+#define RFID_TX_PIN 0
 
 // Configure the Library in UART Mode
 SeeedRFIDLib rfid(RFID_RX_PIN, RFID_TX_PIN);
@@ -47,49 +31,101 @@ DS1307 clock;
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 EthernetClient client;
 
+#define LOOP_INTERVAL 1000L
+long time = 0;
+
+char lcdMessage[16]; 
+
 void setup() {
-	configureLCD();
-	delay(1000);
-	configureRTC();
-	delay(1000);
-	configureEthernet();
+  configureLCD();
+  delay(1000);
+  configureRTC();
+  delay(1000);
+  //configureEthernet();
+  time = millis();
 }
 
 void configureLCD() {
-	// Configures the LCD number of rows and lines
-	lcd.begin(LCD_COLS, LCD_ROWS);
-
-	lcd.clear();
-	lcd.print("LCD OK");
+  lcd.begin();
+  delay(1000);
+  lcd.clear();
+  delay(1000);
+  lcd.print("LCD OK");
 }
 
 void configureRTC() {
-	clock.begin();
-	clock.fillByYMD(2013, 6, 6);
-	clock.fillByHMS(15, 28, 30);
-	clock.fillDayOfWeek(SAT);
-	clock.setTime();
+  clock.begin();
+  clock.fillByYMD(2013, 9, 23);
+  clock.fillByHMS(22, 9, 00);
+  clock.fillDayOfWeek(MON);
+  clock.setTime();
 
-	lcd.clear();
-	lcd.print("RTC OK");
+  lcd.clear();
+  delay(1000);
+  lcd.print("RTC OK");
+  delay(1000);
 }
 
 void configureEthernet() {
-	if (Ethernet.begin(mac) == 0) {
-		lcd.clear();
-		lcd.print("Ethernet ERROR!");
-	}
-	delay(1000);
+  lcd.clear();
+  lcd.print("Ethernet INIT");
+  if (Ethernet.begin(mac) == 0) {
+    lcd.clear();
+    lcd.print("Ethernet ERROR!");
+  }
+  delay(1000);
 
-	lcd.clear();
-	lcd.print("ETHERNET OK");
+  lcd.clear();
+  lcd.print("ETHERNET OK");
 }
 
 void loop() {
-	if (rfid.isIdAvailable()) {
-		tag = rfid.readId();
+  if (rfid.isIdAvailable()) {
+    tag = rfid.readId();
 
-		lcd.clear();
-		lcd.print("ID: " + tag.id);
-	}
+    lcd.clear();
+    lcd.print("ID: " + tag.id);
+  } else {
+    if (millis() - time >= LOOP_INTERVAL) {
+      lcd.clear();
+      clock.getTime();
+      
+      imprimeData();
+      time = millis();
+    }
+  }
 }
+  
+void imprimeData() {
+  lcd.setCursor(0, 0);
+  
+  switch (clock.dayOfWeek) {
+    case MON: {
+      lcd.print("SEG   ");
+    } break;
+    case TUE: {
+      lcd.print("TER   ");
+    } break;
+    case WED: {
+      lcd.print("QUA   ");
+    } break;
+    case THU: {
+      lcd.print("QUI   ");
+    } break;
+    case FRI: {
+      lcd.print("SEX   ");
+    } break;
+    case SAT: {
+      lcd.print("SAB   ");
+    } break;
+    case SUN: {
+      lcd.print("DOM   ");
+    } break;
+  }
+  sprintf(lcdMessage, "%02d/%02d/%04d", clock.dayOfMonth, clock.month, clock.year + 2000);
+  lcd.print(lcdMessage);
+  lcd.setCursor(0, 1);
+  sprintf(lcdMessage, "%02d:%02d:%02d", clock.hour, clock.minute, clock.second);
+  lcd.print(lcdMessage);
+}
+
