2490ea597f7374e96db1caf846bc3d52a8a83292
Gustavo Berdugo <gberdugo@gmail.com>
2013-12-04 22:52:04 -0200

    Remoção de arquivo errado

--- VPSA_Ponto_Client.ino
+++ VPSA_Ponto_Client.ino
@@ -30,6 +30,8 @@ DS1307 clock;
 /* Ethernet Configuration */
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 EthernetClient client;
+char server[] = "http://vpsa-ponto.herokuapp.com/";    // name address for VPSA-Ponto
+String serverResponse = "";
 
 #define LOOP_INTERVAL 1000L
 long time = 0;
@@ -41,7 +43,7 @@ void setup() {
   delay(1000);
   configureRTC();
   delay(1000);
-  //configureEthernet();
+  configureEthernet();
   time = millis();
 }
 
@@ -82,9 +84,60 @@ void configureEthernet() {
 void loop() {
   if (rfid.isIdAvailable()) {
     tag = rfid.readId();
+    
+    if (client.connect(server, 80)) {
+      lcd.clear();
+      sprintf(lcdMessage, "TAG: %d", tag.id);
+      lcd.print(lcdMessage);
+      lcd.setCursor(0, 1);
+      sprintf(lcdMessage, "REGISTRANDO...");
+      lcd.print(lcdMessage);
+      
+      char dataHora[19];
+      sprintf(dataHora, "%02d/%02d/%04d %02:%02:%02", clock.dayOfMonth, clock.month, clock.year + 2000, clock.hour, clock.minute, clock.second);
+      String data = "nroCartao=" + tag.id;
+      data += "&dataHora=" + String(dataHora);
+      
+      // Make a HTTP request:
+      client.println("POST /registro/efetuar HTTP/1.1");
+      client.println("Host: vpsa-ponto.herokuapp.com");
+      client.println("Connection: close");
+      client.println("Content-Type: application/x-www-form-urlencoded");
+      client.print("Content-Length: ");
+      client.print(data.length());
+      client.println();
+      client.println();
+      client.print(data);
+      
+      serverResponse = "";
+      delay(200);
+      while(client.connected() && !client.available()) {
+        delay(1); 
+      }
+      
+      while (client.available()) {
+        serverResponse += client.read();
+      }
 
-    lcd.clear();
-    lcd.print("ID: " + tag.id);
+      client.stop();
+      client.flush();
+      
+      String funcionario = serverResponse.substring(serverResponse.indexOf("\n\n"));
+      
+      lcd.clear();
+      sprintf(lcdMessage, "%s", funcionario);
+      lcd.print(lcdMessage);
+      lcd.setCursor(0, 1);
+      sprintf(lcdMessage, "REGISTRO SALVO");
+      lcd.print(lcdMessage);
+    } else {
+      lcd.clear();
+      sprintf(lcdMessage, "ERRO AO CONECTAR");
+      lcd.print(lcdMessage);
+      lcd.setCursor(0, 1);
+      sprintf(lcdMessage, "NO SERVIDOR");
+      lcd.print(lcdMessage);
+    }
   } else {
     if (millis() - time >= LOOP_INTERVAL) {
       lcd.clear();
@@ -128,4 +181,3 @@ void imprimeData() {
   sprintf(lcdMessage, "%02d:%02d:%02d", clock.hour, clock.minute, clock.second);
   lcd.print(lcdMessage);
 }
-
